b8a754a7b8d2ec1795cdd87c4ae19af8c3bf1bf8
celestiahall44 <celestia.hall44@gmail.com>
2026-02-24 08:26:50 -0700

    tuesday

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+const moviesListEl = document.querySelector(".movie-list");
+
+async function main() {
+    const movies = await fetch ("https://omdbapi.com/?s=fast&apikey=1026992b");
+    const moviesData = await movies.json();
+    moviesListEl.innerHTML = moviesData.map((movie) => movieHTML(movie)).join('');
+}
+
+main();
+
+function movieHTML(movie) {
+    return `div class="movie-card">
+    <figure>
+        <img src="${movie.Poster}" alt="${movie.Title}" class="movie-card__image">
+    </figure>
+    <h3 class="movie-card__title">${movie.Title}</h3>
+    <p class="movie-card__year">Release Year: ${movie.Year}</p>
+</div>`;
+}
+
 function renderMovies() {
     const moviesWrapper = document.querySelector('.movies');
 
@@ -96,8 +116,5 @@ function getMovies() {
       "Type": "movie",
       "Poster": "https://m.media-amazon.com/images/M/MV5BMWM4NTc3N2YtMjk2Ny00MTRmLWE4YzItNTVhMTRlODVkNmE5XkEyXkFqcGc@._V1_SX300.jpg"
     }
-  ],
-  "totalResults": "947",
-  "Response": "True"
-    
+  ];
 }
